b05ddd6e8296fef189b481abbdff287da27b7994
KurtOleh <kurt.volleyball2014@gmail.com>
2018-10-22 00:47:18 +0300

    22.10.2018

---
+++ MyGroup/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyGroupConfig(AppConfig):
+    name = 'MyGroup'
---
+++ MyGroup/static/js/CheckIn.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("MyCheck_in");
+var btn = document.getElementById("Register");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
